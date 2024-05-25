ff15ffbb39a0ccaf11ec325768ca9415b5ba1a59
riccio1234 <125749918+riccio1234@users.noreply.github.com>
2024-05-25 22:27:36 +0200

    Sus
    Sus

---
+++ script.js
@@ -0,0 +1,16 @@
+function openWebSite(url, page) {
+    window.open(url, page)
+}
+
+function start () {
+
+    let splash = document.getElementById("schermatainizio")
+
+    splash.classList.add("hide")
+
+    splash.addEventListener("transitionend", () => {
+
+        document.getElementById("bgm").play();
+        splash.remove();
+    });
+}
