17b7568f2c1ed96ca431bce625918dd0e1011e1f
IL_RICCIO <125749918+riccio1234@users.noreply.github.com>
2024-09-29 00:47:38 +0200

    update

--- js/presence.js
+++ js/presence.js
@@ -1,26 +1,39 @@
 document.addEventListener('DOMContentLoaded', async () => {
     
-    let info = {
-        status: 'on | lime',
+    //fetch('https://localhost:10397/profileApi/').then(response => response.json()).then(info => {
         
-        activityImage: 'https://cdn.discordapp.com/attachments/1135172213507629087/1282270121817145416/Nuovo_progetto_28.png?ex=66ee909c&is=66ed3f1c&hm=d7c937cef1e0f24d15b4618fbdb1ec0f7296a2c373b69370a81c2d01334b7d0d&',
-        text1: 'Sta Giocando a WildMC',
-        text2: 'Best server EU!',
-    };
+        let info = {
+            "visits": "115",
+            "status": "online",
+            "presence": {
+                "img": "https://cdn.discordapp.com/app-assets/383226320970055681/808841241142755358.webp",
+                "text1": "Visual Studio Code",
+                "text2": "Workspace: profile-main"
+            }
+        }
 
-    if (info.status && !info.status.includes('off')) {
-        document.getElementById('presenceStatus').style.backgroundColor = info.status.split(' | ')[1];
-        document.getElementById('presenceImg').src = info.activityImage;
-        document.getElementById('activityText1').textContent = info.text1;
-        document.getElementById('activityText2').textContent = info.text2;
-    } else if (info.status && info.status.includes('off')) {
-        document.getElementById('activitybox').style.visibility = 'hidden';
-    } else {
-        document.getElementById('activitybox').style.visibility = 'hidden';
-        document.getElementById('presenceStatus').style.visibility = 'hidden';
-    };
+        console.log(info.visits);
+
+        if (info.status && !info.status.includes('off')) {
+            document.getElementById('presenceStatus').style.backgroundColor = await getColor(info.status);
+            document.getElementById('presenceImg').src = info.presence.img;
+            document.getElementById('activityText1').textContent = info.presence.text1;
+            document.getElementById('activityText2').textContent = info.presence.text2;
+        } else if (info.status && info.status.includes('off')) {
+            document.getElementById('activitybox').style.visibility = 'hidden';
+        } else {
+            document.getElementById('activitybox').style.visibility = 'hidden';
+            document.getElementById('presenceStatus').style.visibility = 'hidden';
+        };
+    //});
 
 });
+
+async function getColor(status) {
+
+    if (status == 'online') return 'lime';
+
+}
 
 /*
 fetch('/').then(response => response.json()).then(data => {
